7ddbef109797e9271ca46f48b73a02d02f8d8b39
Lea Nguyen <julia.nguyen0616@gmail.com>
2022-07-29 13:29:08 +0700

    init project

---
+++ screens/MainLayout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const MainLayout = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>MavdvdvinLayfsdfsout</Text>
+    </View>
+  );
+};
+
+export default MainLayout;
